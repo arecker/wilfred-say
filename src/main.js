539874b210eaaad7cfeb7b2242575282201b1544
Alex Recker <alex@reckerfamily.com>
2015-05-02 23:36:27 -0500

    Formatting: Cleaned up tabs and indenting

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 (function(quotes){
     'use strict';
     var r = Math.floor(Math.random() * quotes.length),
-	quote = quotes[r],
-	output = '{0}\n-- {1}'.format(quote.t, quote.a);
+    quote = quotes[r],
+    output = '{0}\n-- {1}'.format(quote.t, quote.a);
     console.log(output);
 }(quotes));
