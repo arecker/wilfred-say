82c640a40275471eee8f01afaf85056a1e67596b
Alex Recker <alex@reckerfamily.com>
2015-05-02 23:29:22 -0500

    Initial commit

---
+++ gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+	pkg: grunt.file.readJSON('package.json'),
+
+	uglify: {
+	    options: {
+		banner: '/**\n' +
+		    ' * <%= pkg.name %> - v<%= pkg.version %> \n' +
+		    ' * <%= pkg.description %> \n' +
+		    ' * <%= pkg.repository.url %> \n' +
+		    ' **/\n',
+		mangle: true
+	    },
+	    my_target: {
+		files: {
+		    'wilfred-say.js': ['src/quotes.js', 'src/prototypes.js', 'src/main.js']
+		}
+	    }
+	},
+	jshint: {
+	    all: ['src/*'],
+	    options: {
+		curly: true,
+		eqeqeq: true,
+		eqnull: true,
+		camelcase: true,
+		forin: true,
+		funcscope: true,
+		latedef: true,
+		maxparams: 5,
+		futurehostile: true
+	    }
+	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
